be064c48fb3b33fd691e4879701fbaa33e7bdba3
Ihor_Haraniuk <igorgaranyuk133@gmail.com>
2024-09-19 15:46:22 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Profile from "./components/Profile/Profile";
+import Profile from "./components/Profile/Profil";
 import userData from "./userData.json";
 import FriendList from "./components/FriendList/FriendList";
 import friends from "./friends.json";
